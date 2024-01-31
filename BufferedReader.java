e1881f3f77be1cd1330a22d754fee29bf29e086e
Dipika-Badadhe <157483969+Dipika-Badadhe@users.noreply.github.com>
2024-01-31 12:02:56 +0530

    Add files via upload

---
+++ BufferedReader.java
@@ -0,0 +1,33 @@
+
+// program in BufferedReader
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+public class BuffReader {
+
+    public static void main(String[] args)
+
+        throws IOException
+    {
+
+        BufferedReader reader = new BufferedReader(
+
+            new InputStreamReader(System.in));
+ 
+
+        String name = reader.readLine();
+ 
+        System.out.println(name);
+
+    }
+}
+
+
+
+
+
+
