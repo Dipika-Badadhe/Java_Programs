5d15a210c5bb948322bcfbf89dd38a8819c7ac79
Dipika-Badadhe <157483969+Dipika-Badadhe@users.noreply.github.com>
2024-02-16 14:25:48 +0530

    Add files via upload

--- FileReader.java
+++ FileReader.java
@@ -1,19 +1,20 @@
-package JAVA_A;
-import java.io.*;
-import java.util.*;
+package javaprogram;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class file__reader {
+public class FileReader {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		FileReader filereader = new FileReader("F://filereader.txt");
-		Scanner sc = new Scanner(filereader);
-		
-		while(sc.hasNext())
+		//try with resources
+		try(FileReader fr=new FileReader("D://MYSQL.txt")) {
+        int i;
+        	while((i=fr.read())!=-1){
+        		System.out.print((char)i);   
+        	}}
+		catch(IOException e)
 		{
-			System.out.println(sc.next());
+			System.out.println(e.getMessage());
 		}
-		sc.close();
 	}
-
 }
